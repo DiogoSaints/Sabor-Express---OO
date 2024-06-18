1fcf3a201d9dd33ac8e85e39892f202c945dbdc5
Diogo Santos <diogo.s.santos31@gmail.com>
2024-06-18 20:17:21 -0300

    modificações

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ restaurante_sushi = Restaurante('sushi','japonesa')
 restaurante_praca = Restaurante('Praca','Gourmet')
 resttaurante_piza = Restaurante('Pizza','Italiana')
 
+restaurante_praca.receber_avaliacao('Gui',10)
+restaurante_praca.receber_avaliacao('Gu',5)
 
 
 
---
+++ modelos/avaliacao.py
@@ -0,0 +1,4 @@
+class Avaliacao:
+    def __init__(self, _clente, _nota):
+        self._cliente = _clente
+        self._nota = _nota
